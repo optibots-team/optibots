910c8105d0fe488c47a00cb5fb1c4f9e5be91d1d
Vlad <vladyslav.poliakov.work@gmail.com>
2025-05-22 21:33:19 +0300

    feat(SectionAdvantages): done

---
+++ src/app/[locale]/(pages)/(home)/ui/SectionAdvantages/SectionAdvantages/SectionAdvantages.tsx
@@ -0,0 +1,35 @@
+import { useTranslations } from 'next-intl';
+import { classNames } from '@shared/lib/classNames';
+import { Container } from '@shared/ui/Container';
+import { SectionTitle } from '@features/SectionTitle';
+import { AdvantagesList } from '../AdvantagesList/AdvantagesList';
+import { TextColor } from '@shared/ui/Text';
+import styles from './SectionAdvantages.module.scss';
+import AtomIcon from '@shared/assets/icons/atom_gradient.svg';
+
+type SectionAdvantagesProps = {
+	className?: string;
+};
+
+const SectionAdvantages = ({ className }: SectionAdvantagesProps) => {
+	const t = useTranslations('homePage.sectionAdvantages');
+
+	return (
+		<section className={classNames(styles.advantages, {}, [className, 'section-padding'])}>
+			<Container display={'flex'} orientation={'vertical'} className={styles.advantages__container}>
+				<SectionTitle
+					className={styles.advantages__title}
+					badgeBordered
+					badgeIcon={<AtomIcon />}
+					badgeText={t('badge')}
+					title={t('sectionTitle')}
+					desc={t.rich('sectionDesc', { semiBold: (chunks) => <span className={'fw-600'}>{chunks}</span> })}
+					textColor={TextColor.DARK}
+				/>
+				<AdvantagesList />
+			</Container>
+		</section>
+	);
+};
+
+export { SectionAdvantages };
