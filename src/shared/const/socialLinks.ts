a4bfb5eef33856971e5c3710c49154b99d8ae49b
Vlad <vladyslav.poliakov.work@gmail.com>
2025-06-24 19:30:12 +0300

    feat: links for socials && fix mobile bug & refactor marquee

--- src/shared/const/socialLinks.ts
+++ src/shared/const/socialLinks.ts
@@ -1,6 +1,6 @@
 export const socialLinks = {
-	TELEGRAM: 'https://localhost:8080',
-	INSTAGRAM: 'https://localhost:8080',
-	LINKEDIN: 'https://localhost:8080',
-	YOUTUBE: 'https://localhost:8080',
+	TELEGRAM: 'https://t.me/+ZsUzLNAC2605OTgy',
+	INSTAGRAM: 'https://www.instagram.com/opti_bots?igsh=MWV3dTVtaTNmMm1wcg==',
+	LINKEDIN: 'https://www.linkedin.com/in/opti-bots-495333370',
+	YOUTUBE: 'https://www.youtube.com/channel/UCaOfiv3j-rHjiGh2-lWnoxA',
 } as const;
